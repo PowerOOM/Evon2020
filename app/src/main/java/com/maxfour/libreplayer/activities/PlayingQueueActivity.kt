e8b7d95b2adb25f30534a6259903ff62394166fc
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-06-13 06:47:54 +0000

    Fixed type error

--- app/src/main/java/com/maxfour/libreplayer/activities/PlayingQueueActivity.kt
+++ app/src/main/java/com/maxfour/libreplayer/activities/PlayingQueueActivity.kt
@@ -57,3 +57,141 @@ open class PlayingQueueActivity : AbsMusicServiceActivity() {
 		}
 		checkForPadding()
 	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			android.R.id.home -> {
+				onBackPressed()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
+	}
+
+	private fun setUpRecyclerView() {
+		recyclerViewTouchActionGuardManager = RecyclerViewTouchActionGuardManager()
+		recyclerViewDragDropManager = RecyclerViewDragDropManager()
+		recyclerViewSwipeManager = RecyclerViewSwipeManager()
+
+		val animator = DraggableItemAnimator()
+		animator.supportsChangeAnimations = false
+
+		playingQueueAdapter = PlayingQueueAdapter(
+			this,
+			MusicPlayerRemote.playingQueue,
+			MusicPlayerRemote.position,
+			R.layout.item_queue
+		)
+		wrappedAdapter = recyclerViewDragDropManager?.createWrappedAdapter(playingQueueAdapter!!)
+		wrappedAdapter = wrappedAdapter?.let { recyclerViewSwipeManager?.createWrappedAdapter(it) }
+
+		linearLayoutManager = LinearLayoutManager(this)
+
+		recyclerView.layoutManager = linearLayoutManager
+		recyclerView.adapter = wrappedAdapter
+		recyclerView.itemAnimator = animator
+		recyclerViewTouchActionGuardManager?.attachRecyclerView(recyclerView)
+		recyclerViewDragDropManager?.attachRecyclerView(recyclerView)
+		recyclerViewSwipeManager?.attachRecyclerView(recyclerView)
+		linearLayoutManager.scrollToPositionWithOffset(MusicPlayerRemote.position + 1, 0)
+
+		recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+				super.onScrolled(recyclerView, dx, dy)
+				if (dy > 0) {
+					clearQueue.shrink()
+				} else if (dy < 0) {
+					clearQueue.extend()
+				}
+			}
+		})
+		ViewUtil.setUpFastScrollRecyclerViewColor(this, recyclerView)
+	}
+
+	private fun checkForPadding() {
+	}
+
+	override fun onQueueChanged() {
+		if (MusicPlayerRemote.playingQueue.isEmpty()) {
+			finish()
+			return
+		}
+		checkForPadding()
+		updateQueue()
+		updateCurrentSong()
+	}
+
+	override fun onMediaStoreChanged() {
+		updateQueue()
+		updateCurrentSong()
+	}
+
+	private fun updateCurrentSong() {
+		playerQueueSubHeader.text = getUpNextAndQueueTime()
+	}
+
+	override fun onPlayingMetaChanged() {
+		updateQueuePosition()
+	}
+
+	private fun updateQueuePosition() {
+		playingQueueAdapter?.setCurrent(MusicPlayerRemote.position)
+		resetToCurrentPosition()
+		playerQueueSubHeader.text = getUpNextAndQueueTime()
+	}
+
+	private fun updateQueue() {
+		playingQueueAdapter?.swapDataSet(MusicPlayerRemote.playingQueue, MusicPlayerRemote.position)
+		resetToCurrentPosition()
+	}
+
+	private fun resetToCurrentPosition() {
+		recyclerView.stopScroll()
+		linearLayoutManager.scrollToPositionWithOffset(MusicPlayerRemote.position + 1, 0)
+	}
+
+	override fun onPause() {
+		if (recyclerViewDragDropManager != null) {
+			recyclerViewDragDropManager!!.cancelDrag()
+		}
+		super.onPause()
+	}
+
+	override fun onDestroy() {
+		if (recyclerViewDragDropManager != null) {
+			recyclerViewDragDropManager!!.release()
+			recyclerViewDragDropManager = null
+		}
+		if (recyclerViewSwipeManager != null) {
+			recyclerViewSwipeManager?.release()
+			recyclerViewSwipeManager = null
+		}
+		if (wrappedAdapter != null) {
+			WrapperAdapterUtils.releaseAll(wrappedAdapter)
+			wrappedAdapter = null
+		}
+		playingQueueAdapter = null
+		super.onDestroy()
+	}
+
+	private fun setupToolbar() {
+		playerQueueSubHeader.text = getUpNextAndQueueTime()
+		playerQueueSubHeader.setTextColor(ThemeStore.accentColor(this))
+
+		applyToolbar(toolbar)
+		clearQueue.backgroundTintList = ColorStateList.valueOf(ThemeStore.accentColor(this))
+		ColorStateList.valueOf(
+				MaterialValueHelper.getPrimaryTextColor(
+						this,
+						ColorUtil.isColorLight(
+								ThemeStore.accentColor(
+										this
+								)
+						)
+				)
+		).apply {
+			clearQueue.setTextColor(this)
+			clearQueue.iconTint = this
+		}
+	}
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/activities/base/AbsBaseActivity.kt
@@ -0,0 +1,9 @@
+package com.maxfour.libreplayer.activities.base
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.net.Uri
+import android.os.Build
+import
