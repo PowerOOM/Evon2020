7bf6f5407194bf82d6f4d7a359485077ff33ac19
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-02 18:20:23 +0000

    Fixed version number.

--- app/src/main/java/com/maxfour/libreplayer/activities/saf/SAFGuideActivity.java
+++ app/src/main/java/com/maxfour/libreplayer/activities/saf/SAFGuideActivity.java
@@ -2,4 +2,34 @@ package com.maxfour.libreplayer.activities.saf;
 
 import android.os.Build;
 import android.os.Bundle;
-import androidx.anno
+import androidx.annotation.Nullable;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+import com.maxfour.libreplayer.R;
+
+public class SAFGuideActivity extends IntroActivity {
+
+    public static final int REQUEST_CODE_SAF_GUIDE = 98;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setButtonCtaVisible(false);
+        setButtonNextVisible(false);
+        setButtonBackVisible(false);
+
+        setButtonCtaTintMode(BUTTON_CTA_TINT_MODE_TEXT);
+
+        String title = String.format(getString(R.string.saf_guide_slide1_title), getString(R.string.app_name));
+
+        addSlide(new SimpleSlide.Builder()
+                .title(title)
+                .description(Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1
+                        ? R.string.saf_guide_slide1_description_before_o : R.string.saf_guide_slide1_description)
+                .image(R.drawable.saf_guide_1)
+                .background(R.color.md_deep_purple_300)
+                .backgroundDark(R.color.md_deep_purple_400)
+                .layout(R.layout.fragment_simple_slide_large_image)
+                .build());
+        addSlide(new Simpl
