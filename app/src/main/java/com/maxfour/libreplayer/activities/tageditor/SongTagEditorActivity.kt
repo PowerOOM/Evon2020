a5c975ef07d101716f038157192a7069c5995f83
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-09 08:44:18 +0000

    Fixed some compile warnings

--- app/src/main/java/com/maxfour/libreplayer/activities/tageditor/SongTagEditorActivity.kt
+++ app/src/main/java/com/maxfour/libreplayer/activities/tageditor/SongTagEditorActivity.kt
@@ -57,4 +57,43 @@ class SongTagEditorActivity : AbsTagEditorActivity(), TextWatcher {
 		genreText.setText(genreName)
 		yearText.setText(songYear)
 		songNumberText.setText(songNumber)
-		lyricsText.setText(lyr
+		lyricsText.setText(lyrics)
+		songComposerText.setText(composer)
+	}
+
+	override fun loadCurrentImage() {
+	}
+
+	override fun searchImageOnWeb() {
+	}
+
+	override fun deleteImage() {
+	}
+
+	override fun save() {
+		val fieldKeyValueMap = EnumMap<FieldKey, String>(FieldKey::class.java)
+		fieldKeyValueMap[FieldKey.TITLE] = songText.text.toString()
+		fieldKeyValueMap[FieldKey.ALBUM] = albumText.text.toString()
+		fieldKeyValueMap[FieldKey.ARTIST] = artistText.text.toString()
+		fieldKeyValueMap[FieldKey.GENRE] = genreText.text.toString()
+		fieldKeyValueMap[FieldKey.YEAR] = yearText.text.toString()
+		fieldKeyValueMap[FieldKey.TRACK] = songNumberText.text.toString()
+		fieldKeyValueMap[FieldKey.LYRICS] = lyricsText.text.toString()
+		fieldKeyValueMap[FieldKey.ALBUM_ARTIST] = albumArtistText.text.toString()
+		fieldKeyValueMap[FieldKey.COMPOSER] = songComposerText.text.toString()
+		writeValuesToFiles(fieldKeyValueMap, null)
+	}
+
+	override fun getSongPaths(): List<String> {
+		val paths = ArrayList<String>(1)
+		paths.add(SongLoader.getSong(this, id).data)
+		return paths
+	}
+
+	override fun loadImageFromFile(selectedFile: Uri?) {
+	}
+
+	override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+	}
+
+	override fun onTextChanged(s: CharSequence, start: Int, before:
