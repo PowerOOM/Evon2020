48f4d91bd4f89749719b00c5ef7db00ae9305685
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-13 08:10:55 +0000

    General updates.

--- app/src/main/java/com/maxfour/libreplayer/adapter/album/AlbumFullWidthAdapter.kt
+++ app/src/main/java/com/maxfour/libreplayer/adapter/album/AlbumFullWidthAdapter.kt
@@ -21,4 +21,44 @@ class AlbumFullWidthAdapter(
 	metrics: DisplayMetrics
 ) : MetalRecyclerViewPager.MetalAdapter<AlbumFullWidthAdapter.FullMetalViewHolder>(metrics) {
 
-	override fun onCreateViewHolder(parent
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FullMetalViewHolder {
+		return FullMetalViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.pager_item, parent, false))
+	}
+
+	override fun onBindViewHolder(holder: FullMetalViewHolder, position: Int) {
+		// don't forget about calling supper.onBindViewHolder!
+		super.onBindViewHolder(holder, position)
+		val album = dataSet[position]
+		holder.title?.text = getAlbumTitle(album)
+		holder.text?.text = getAlbumText(album)
+		holder.playSongs?.setOnClickListener {
+			album.songs?.let { songs ->
+				MusicPlayerRemote.openQueue(
+						songs,
+						0,
+						true
+				)
+			}
+		}
+		loadAlbumCover(album, holder)
+	}
+
+	private fun getAlbumTitle(album: Album): String? {
+		return album.title
+	}
+
+	private fun getAlbumText(album: Album): String? {
+		return album.artistName
+	}
+
+	private fun loadAlbumCover(album: Album, holder: FullMetalViewHolder) {
+		if (holder.image == null) {
+			return
+		}
+		SongGlideRequest.Builder.from(Glide.with(activity), album.safeGetFirstSong())
+			.checkIgnoreMediaStore(activity)
+			.generatePalette(activity)
+			.build()
+			.into(object : PlayerColoredTarget(holder.image!!) {
+				override fun onColorReady(color: Int) {
+	
