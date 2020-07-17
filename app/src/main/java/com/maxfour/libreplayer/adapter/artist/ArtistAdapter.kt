65a39f8b52a545fd67441b88a02636b3f1fd518f
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-17 06:02:03 +0000

    v1.10.0

--- app/src/main/java/com/maxfour/libreplayer/adapter/artist/ArtistAdapter.kt
+++ app/src/main/java/com/maxfour/libreplayer/adapter/artist/ArtistAdapter.kt
@@ -15,4 +15,39 @@ import com.maxfour.libreplayer.R
 import com.maxfour.libreplayer.adapter.base.AbsMultiSelectAdapter
 import com.maxfour.libreplayer.adapter.base.MediaEntryViewHolder
 import com.maxfour.libreplayer.glide.ArtistGlideRequest
-import com.maxfour.libreplayer.glide.PlayerC
+import com.maxfour.libreplayer.glide.PlayerColoredTarget
+import com.maxfour.libreplayer.helper.menu.SongsMenuHelper
+import com.maxfour.libreplayer.interfaces.CabHolder
+import com.maxfour.libreplayer.model.Artist
+import com.maxfour.libreplayer.model.Song
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.NavigationUtil
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
+import java.util.ArrayList
+
+class ArtistAdapter(
+	val activity: AppCompatActivity,
+	var dataSet: ArrayList<Artist>,
+	var itemLayoutRes: Int,
+	var usePalette: Boolean,
+	cabHolder: CabHolder?
+) : AbsMultiSelectAdapter<ArtistAdapter.ViewHolder, Artist>(
+	activity, cabHolder, R.menu.menu_media_selection
+), FastScrollRecyclerView.SectionedAdapter {
+
+	fun swapDataSet(dataSet: ArrayList<Artist>) {
+		this.dataSet = dataSet
+		notifyDataSetChanged()
+	}
+
+	fun usePalette(usePalette: Boolean) {
+		this.usePalette = usePalette
+		notifyDataSetChanged()
+	}
+
+	override fun getItemId(position: Int): Long {
+		return dataSet[position].id.toLong()
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val view = LayoutInflater.from(activity).inflate(itemLayoutRes, pare
