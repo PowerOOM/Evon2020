06339689f40e31595387af3e71a12d1ea6a83ad0
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-22 12:54:57 +0000

    Update to 9715cdc from source

--- app/src/main/java/com/maxfour/libreplayer/adapter/base/AbsMultiSelectAdapter.java
+++ app/src/main/java/com/maxfour/libreplayer/adapter/base/AbsMultiSelectAdapter.java
@@ -24,4 +24,45 @@ public abstract class AbsMultiSelectAdapter<VH extends RecyclerView.ViewHolder, 
     private ArrayList<I> checked;
     private int menuRes;
 
-    public AbsMultiS
+    public AbsMultiSelectAdapter(@NonNull Context context, @Nullable CabHolder cabHolder, @MenuRes int menuRes) {
+        this.cabHolder = cabHolder;
+        checked = new ArrayList<>();
+        this.menuRes = menuRes;
+        this.context = context;
+    }
+
+    protected void setMultiSelectMenuRes(@MenuRes int menuRes) {
+        this.menuRes = menuRes;
+    }
+
+    protected boolean toggleChecked(final int position) {
+        if (cabHolder != null) {
+            I identifier = getIdentifier(position);
+            if (identifier == null) return false;
+
+            if (!checked.remove(identifier)) checked.add(identifier);
+
+            notifyItemChanged(position);
+            updateCab();
+            return true;
+        }
+        return false;
+    }
+
+    protected void checkAll() {
+        if (cabHolder != null) {
+            checked.clear();
+            for (int i = 0; i < getItemCount(); i++) {
+                I identifier = getIdentifier(i);
+                if (identifier != null) {
+                    checked.add(identifier);
+                }
+            }
+            notifyDataSetChanged();
+            updateCab();
+        }
+    }
+
+    private void updateCab() {
+        if (cabHolder != null) {
+            if (cab == null || !c
