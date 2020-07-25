162f66391899d78b7c9ef60012fc3c9f04f28f5c
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-25 10:38:38 +0000

    \ufeff ChangeLog: update

--- app/src/main/java/com/maxfour/libreplayer/adapter/song/AbsOffsetSongAdapter.kt
+++ app/src/main/java/com/maxfour/libreplayer/adapter/song/AbsOffsetSongAdapter.kt
@@ -18,4 +18,45 @@ abstract class AbsOffsetSongAdapter : SongAdapter {
 			dataSet: ArrayList<Song>, @LayoutRes itemLayoutRes: Int,
 			usePalette: Boolean,
 			cabHolder: CabHolder?
-	) : super(ac
+	) : super(activity, dataSet, itemLayoutRes, usePalette, cabHolder)
+
+	constructor(
+			activity: AppCompatActivity,
+			dataSet: ArrayList<Song>, @LayoutRes itemLayoutRes: Int,
+			usePalette: Boolean,
+			cabHolder: CabHolder?,
+			showSectionName: Boolean
+	) : super(activity, dataSet, itemLayoutRes, usePalette, cabHolder, showSectionName) {
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongAdapter.ViewHolder {
+		if (viewType == OFFSET_ITEM) {
+			val view = LayoutInflater.from(activity)
+				.inflate(R.layout.item_list_quick_actions, parent, false)
+			return createViewHolder(view)
+		}
+		return super.onCreateViewHolder(parent, viewType)
+	}
+
+	override fun createViewHolder(view: View): SongAdapter.ViewHolder {
+		return ViewHolder(view)
+	}
+
+	override fun getItemId(position: Int): Long {
+		var positionFinal = position
+		positionFinal--
+		return if (positionFinal < 0) -2 else super.getItemId(positionFinal)
+	}
+
+	override fun getIdentifier(position: Int): Song? {
+		var positionFinal = position
+		positionFinal--
+		return if (positionFinal < 0) null else super.getIdentifier(positionFinal)
+	}
+
+	override fun getItemCount(): Int {
+		val superItemCount = super.getItemCount()
+		return if (superItemCount == 0) 0 else superItemCount + 1
+	}
+
+	
