4dd0df2dc0225e0743b832dae9e6f4fa9f3bc612
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-25 21:34:01 +0000

    delete user

--- app/src/main/java/com/maxfour/libreplayer/adapter/song/SongAdapter.kt
+++ app/src/main/java/com/maxfour/libreplayer/adapter/song/SongAdapter.kt
@@ -15,4 +15,40 @@ import com.maxfour.libreplayer.adapter.base.MediaEntryViewHolder
 import com.maxfour.libreplayer.extensions.hide
 import com.maxfour.libreplayer.extensions.show
 import com.maxfour.libreplayer.glide.PlayerColoredTarget
-import
+import com.maxfour.libreplayer.glide.SongGlideRequest
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.helper.SortOrder
+import com.maxfour.libreplayer.helper.menu.SongMenuHelper
+import com.maxfour.libreplayer.helper.menu.SongsMenuHelper
+import com.maxfour.libreplayer.interfaces.CabHolder
+import com.maxfour.libreplayer.model.Song
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.NavigationUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
+import java.util.ArrayList
+
+open class SongAdapter(
+	protected val activity: AppCompatActivity,
+	dataSet: ArrayList<Song>,
+	protected var itemLayoutRes: Int,
+	usePalette: Boolean,
+	cabHolder: CabHolder?,
+	showSectionName: Boolean = true
+) : AbsMultiSelectAdapter<SongAdapter.ViewHolder, Song>(
+	activity, cabHolder, R.menu.menu_media_selection
+), MaterialCab.Callback, FastScrollRecyclerView.SectionedAdapter {
+
+	var dataSet: ArrayList<Song>
+
+	protected var usePalette = false
+	private var showSectionName = true
+
+	init {
+		this.dataSet = dataSet
+		this.usePalette = usePalette
+		this.showSectionName = showSectionName
+		this.setHasStableIds(true)
+	}
+
+	ope
