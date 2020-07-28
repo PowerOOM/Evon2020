df756807edf1060b2e1e0d9067ebc4aa1ea383a0
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-07-28 07:02:38 +0000

    Rebuild app

--- app/src/main/java/com/maxfour/libreplayer/adapter/song/SongAdapter.kt
+++ app/src/main/java/com/maxfour/libreplayer/adapter/song/SongAdapter.kt
@@ -51,4 +51,44 @@ open class SongAdapter(
 		this.setHasStableIds(true)
 	}
 
-	ope
+	open fun swapDataSet(dataSet: ArrayList<Song>) {
+		this.dataSet = dataSet
+		notifyDataSetChanged()
+	}
+
+	open fun usePalette(usePalette: Boolean) {
+		this.usePalette = usePalette
+		notifyDataSetChanged()
+	}
+
+	override fun getItemId(position: Int): Long {
+		return dataSet[position].id.toLong()
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val view = LayoutInflater.from(activity).inflate(itemLayoutRes, parent, false)
+		return createViewHolder(view)
+	}
+
+	protected open fun createViewHolder(view: View): ViewHolder {
+		return ViewHolder(view)
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		val song = dataSet[position]
+		val isChecked = isChecked(song)
+		holder.itemView.isActivated = isChecked
+		if (isChecked) {
+			holder.menu?.hide()
+		} else {
+			holder.menu?.show()
+		}
+		holder.title?.text = getSongTitle(song)
+		holder.text?.text = getSongText(song)
+		loadAlbumCover(song, holder)
+	}
+
+	private fun setColors(color: Int, holder: ViewHolder) {
+		if (holder.paletteColorContainer != null) {
+			holder.paletteColorContainer?.setBackgroundColor(color)
+			//holder.title?.setTextColor(MaterialValueHelper.getPrimaryTextColor(activity, C
