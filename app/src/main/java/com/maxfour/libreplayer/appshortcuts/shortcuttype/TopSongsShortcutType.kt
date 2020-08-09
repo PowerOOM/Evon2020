4e4b530671d07d6f81cbcab9a302dd48981ec4eb
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-08-09 11:31:05 +0000

    Remove useless variables

---
+++ app/src/main/java/com/maxfour/libreplayer/appshortcuts/shortcuttype/TopSongsShortcutType.kt
@@ -0,0 +1,9 @@
+package com.maxfour.libreplayer.appshortcuts.shortcuttype
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.pm.ShortcutInfo
+import android.os.Build
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.appshortcuts.AppShortcutIconGenerator
+import com.maxfour.libreplayer.appshortcuts.AppShortcutL
