024893802dd78464b6c879fb2b6977a8a42f211f
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-08-11 08:08:01 +0000

    Bump tar from 2.2.1 to 2.2.2

---
+++ app/src/main/java/com/maxfour/libreplayer/appwidgets/AppWidgetBig.kt
@@ -0,0 +1,16 @@
+package com.maxfour.libreplayer.appwidgets
+
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.text.TextUtils
+import android.view.View
+import android.widget.RemoteViews
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
+import com.maxfour.a
