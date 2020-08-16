4b67a5daf29edee20cb2abb6420ecfb487d40d8b
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-08-16 06:04:17 +0000

    Initial project files

--- app/src/main/java/com/maxfour/libreplayer/appwidgets/AppWidgetCard.kt
+++ app/src/main/java/com/maxfour/libreplayer/appwidgets/AppWidgetCard.kt
@@ -18,4 +18,35 @@ import com.maxfour.libreplayer.R
 import com.maxfour.libreplayer.activities.MainActivity
 import com.maxfour.libreplayer.appwidgets.base.BaseAppWidget
 import com.maxfour.libreplayer.glide.SongGlideRequest
-import com.maxfou
+import com.maxfour.libreplayer.glide.palette.BitmapPaletteWrapper
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.service.MusicService.*
+import com.maxfour.libreplayer.util.ImageUtil
+import com.maxfour.libreplayer.util.PlayerUtil
+
+class AppWidgetCard : BaseAppWidget() {
+	private var target: Target<BitmapPaletteWrapper>? = null // for cancellation
+
+	/**
+	 * Initialize given widgets to default state, where we launch Music on default click and hide
+	 * actions if service not running.
+	 */
+	override fun defaultAppWidget(context: Context, appWidgetIds: IntArray) {
+		val appWidgetView = RemoteViews(context.packageName, R.layout.app_widget_card)
+
+		appWidgetView.setViewVisibility(R.id.media_titles, View.INVISIBLE)
+		appWidgetView.setImageViewResource(R.id.image, R.drawable.default_album_art)
+		appWidgetView.setImageViewBitmap(
+				R.id.button_next, createBitmap(
+				PlayerUtil.getTintedVectorDrawable(
+						context,
+						R.drawable.ic_skip_next_white_24dp,
+						MaterialValueHelper.getSecondaryTextColor(
+								context, true
+						)
+				)!!, 1f
+		)
+		)
+		appWidgetView.setImageViewBitmap(
+				R.id.button_prev, createBitmap(
+				PlayerUtil.getTintedVector
