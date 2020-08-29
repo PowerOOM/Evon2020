e57b5c9d4d0037e5115f3937ba2190d143dd38fe
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-08-29 13:19:01 +0000

    remove old TODO

--- app/src/main/java/com/maxfour/libreplayer/dialogs/RemoveFromPlaylistDialog.kt
+++ app/src/main/java/com/maxfour/libreplayer/dialogs/RemoveFromPlaylistDialog.kt
@@ -7,4 +7,33 @@ import androidx.fragment.app.DialogFragment
 import com.afollestad.materialdialogs.LayoutMode
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.bottomsheets.BottomSheet
-import com.maxfour.li
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.model.PlaylistSong
+import com.maxfour.libreplayer.util.PlaylistsUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+
+class RemoveFromPlaylistDialog : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val songs = arguments!!.getParcelableArrayList<PlaylistSong>("songs")
+
+        var title = 0
+        var content: CharSequence = ""
+        if (songs != null) {
+            if (songs.size > 1) {
+                title = R.string.remove_songs_from_playlist_title
+                content = Html.fromHtml(getResources().getQuantityString(R.plurals.remove_x_songs_from_playlist, songs.size, songs.size))
+            } else {
+                title = R.string.remove_song_from_playlist_title
+                content = Html.fromHtml(getString(com.maxfour.libreplayer.R.string.remove_song_x_from_playlist, songs[0].title))
+            }
+        }
+
+
+        return MaterialDialog(requireContext(), BottomSheet(LayoutMode.WRAP_CONTENT))
+                .show {
+                    title(title)
+                    message(text = content)
+                    negativeButton(android.R.string.cancel)
+                    positiveButton(R.string.remove_action) {
+                        if (activit
