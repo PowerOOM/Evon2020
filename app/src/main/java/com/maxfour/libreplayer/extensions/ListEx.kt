9b9673c3c39e4f44bf95e5e1a8cbf6470d86dd9e
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-09-04 10:16:36 +0000

    added italian translation

--- app/src/main/java/com/maxfour/libreplayer/extensions/ListEx.kt
+++ app/src/main/java/com/maxfour/libreplayer/extensions/ListEx.kt
@@ -5,4 +5,9 @@ import com.maxfour.libreplayer.model.Song
 
 fun ArrayList<Song>.lastElement(): Boolean {
     println("${this.size} ${this.indexOf(MusicPlayerRemote.currentSong)}")
-    r
+    return this.size - 1 == this.indexOf(MusicPlayerRemote.currentSong)
+}
+
+fun ArrayList<Song>.fistElement(): Boolean {
+    return 0 == this.indexOf(MusicPlayerRemote.currentSong)
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/fragments/MiniPlayerFragment.kt
@@ -0,0 +1,7 @@
+package com.maxfour.libreplayer.fragments
+
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.text.Sp
