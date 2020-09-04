a159b059add0cda596ddcb1ce205243d5112cf83
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-09-04 18:17:36 +0000

    regen HEAD

--- app/src/main/java/com/maxfour/libreplayer/fragments/MiniPlayerFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/MiniPlayerFragment.kt
@@ -4,4 +4,31 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.text.Sp
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.view.*
+import android.view.animation.DecelerateInterpolator
+import com.maxfour.appthemehelper.ThemeStore
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.fragments.base.AbsMusicServiceFragment
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.helper.MusicProgressViewUpdateHelper
+import com.maxfour.libreplayer.helper.PlayPauseButtonOnClickHandler
+import com.maxfour.libreplayer.util.NavigationUtil
+import com.maxfour.libreplayer.util.PlayerUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.maxfour.libreplayer.util.ViewUtil
+import kotlinx.android.synthetic.main.fragment_mini_player.*
+import kotlin.math.abs
+
+open class MiniPlayerFragment : AbsMusicServiceFragment(), MusicProgressViewUpdateHelper.Callback, View.OnClickListener {
+
+    private lateinit var progressViewUpdateHelper: MusicProgressViewUpdateHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        progressViewUpdateHelper = MusicProgressViewUpdateHelper(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: View
