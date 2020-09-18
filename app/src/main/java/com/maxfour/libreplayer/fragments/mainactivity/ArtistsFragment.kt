1cb74c7262b338a2dd8e9c3a4d7da7eea227fa6b
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-09-18 12:28:15 +0000

    Windows build

--- app/src/main/java/com/maxfour/libreplayer/fragments/mainactivity/ArtistsFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/mainactivity/ArtistsFragment.kt
@@ -26,4 +26,44 @@ class ArtistsFragment : AbsLibraryPagerRecyclerViewCustomGridSizeFragment<Artist
     override val emptyMessage: Int
         get() = R.string.no_artists
 
-    override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        App.musicComponent.inject(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        artistsPresenter.attachView(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (adapter!!.dataSet.isEmpty()) {
+            artistsPresenter.loadArtists()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        artistsPresenter.detachView()
+    }
+
+    override fun onMediaStoreChanged() {
+        artistsPresenter.loadArtists()
+    }
+
+    override fun setSortOrder(sortOrder: String) {
+        artistsPresenter.loadArtists()
+    }
+
+    override fun createLayoutManager(): GridLayoutManager {
+        return GridLayoutManager(activity, getGridSize())
+    }
+
+    override fun createAdapter(): ArtistAdapter {
+        var itemLayoutRes = itemLayoutRes
+        notifyLayoutResChanged(itemLayoutRes)
+        if (itemLayoutRes != R.layout.item_list) {
+            itemLayoutRes = PreferenceUtil.getInstance(requireContext()).getArtistGridStyle(requireContext())
+        }
+        val dataSet 
