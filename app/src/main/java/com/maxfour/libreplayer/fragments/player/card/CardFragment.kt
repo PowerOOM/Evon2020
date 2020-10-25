5e464e16374dc2fa29c76f4dcefdc845a54ef3ed
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-10-25 09:32:38 +0000

    Simplified tests

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/card/CardFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/card/CardFragment.kt
@@ -2,4 +2,43 @@ package com.maxfour.libreplayer.fragments.player.card
 
 import android.graphics.Color
 import android.os.Bundle
-import andro
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import com.maxfour.appthemehelper.util.ToolbarContentTintHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.fragments.base.AbsPlayerFragment
+import com.maxfour.libreplayer.fragments.player.PlayerAlbumCoverFragment
+import com.maxfour.libreplayer.fragments.player.normal.PlayerFragment
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.model.Song
+import kotlinx.android.synthetic.main.fragment_card_player.*
+
+class CardFragment : AbsPlayerFragment() {
+    override fun playerToolbar(): Toolbar {
+        return playerToolbar
+    }
+
+    private var lastColor: Int = 0
+    override val paletteColor: Int
+        get() = lastColor
+
+    private lateinit var playbackControlsFragment: CardPlaybackControlsFragment
+
+    override fun onShow() {
+        playbackControlsFragment.show()
+    }
+
+    override fun onHide() {
+        playbackControlsFragment.hide()
+        onBackPressed()
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
+    }
+
+    override fun toolbarIconColor(): Int {
+        return Color.WHITE
+  
