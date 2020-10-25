adaf026be4fc612946a32d06aefac601c4a08340
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-10-25 17:53:36 +0000

    Add features

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/card/CardPlaybackControlsFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/card/CardPlaybackControlsFragment.kt
@@ -9,4 +9,30 @@ import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
 import android.widget.SeekBar
 import com.maxfour.appthemehelper.ThemeStore
-import com.maxfour.appthemehelp
+import com.maxfour.appthemehelper.util.ATHUtil
+import com.maxfour.appthemehelper.util.ColorUtil
+import com.maxfour.appthemehelper.util.MaterialValueHelper
+import com.maxfour.appthemehelper.util.TintHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.extensions.ripAlpha
+import com.maxfour.libreplayer.fragments.base.AbsPlayerControlsFragment
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.helper.MusicProgressViewUpdateHelper
+import com.maxfour.libreplayer.helper.PlayPauseButtonOnClickHandler
+import com.maxfour.libreplayer.misc.SimpleOnSeekbarChangeListener
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import kotlinx.android.synthetic.main.fragment_card_player_playback_controls.*
+import kotlinx.android.synthetic.main.media_button.*
+
+class CardPlaybackControlsFragment : AbsPlayerControlsFragment() {
+
+    private var lastPlaybackControlsColor: Int = 0
+    private var lastDisabledPlaybackControlsColor: Int = 0
+    private var progressViewUpdateHelper: MusicProgressViewUpdateHelper? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        progres
