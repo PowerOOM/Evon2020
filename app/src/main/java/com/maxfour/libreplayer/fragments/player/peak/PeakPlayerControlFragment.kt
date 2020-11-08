ba63f31a9b42dbc635ea352c7fcb08f91bafaa65
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-11-08 12:29:54 +0000

    Add run script

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/peak/PeakPlayerControlFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/peak/PeakPlayerControlFragment.kt
@@ -11,4 +11,28 @@ import android.view.animation.LinearInterpolator
 import android.widget.SeekBar
 import com.maxfour.appthemehelper.ThemeStore
 import com.maxfour.appthemehelper.util.ATHUtil
-import com.maxfour.appthe
+import com.maxfour.appthemehelper.util.ColorUtil
+import com.maxfour.appthemehelper.util.MaterialValueHelper
+import com.maxfour.appthemehelper.util.TintHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.extensions.ripAlpha
+import com.maxfour.libreplayer.fragments.base.AbsPlayerControlsFragment
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.helper.MusicProgressViewUpdateHelper
+import com.maxfour.libreplayer.helper.PlayPauseButtonOnClickHandler
+import com.maxfour.libreplayer.misc.SimpleOnSeekbarChangeListener
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.maxfour.libreplayer.util.ViewUtil
+import kotlinx.android.synthetic.main.fragment_peak_control_player.*
+
+class PeakPlayerControlFragment : AbsPlayerControlsFragment() {
+
+    private lateinit var progressViewUpdateHelper: MusicProgressViewUpdateHelper
+    private var lastPlaybackControlsColor: Int = 0
+    private var lastDisabledPlaybackControlsColor: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        progressViewUpdateHelper = MusicProgressViewUp
