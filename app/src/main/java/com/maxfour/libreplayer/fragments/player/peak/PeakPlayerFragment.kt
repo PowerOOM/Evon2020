627d074795abfc212a19d554d6bcd2bf204a8fbe
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-11-12 17:59:23 +0000

    Remove debugging message

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/peak/PeakPlayerFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/peak/PeakPlayerFragment.kt
@@ -4,4 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import a
+import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import com.maxfour.appthemehelper.util.ATHUtil
+import com.maxfour.appthemehelper.util.ToolbarContentTintHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.fragments.base.AbsPlayerFragment
+import com.maxfour.libreplayer.glide.PlayerColoredTarget
+import com.maxfour.libreplayer.glide.SongGlideRequest
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.util.NavigationUtil
+import kotlinx.android.synthetic.main.fragment_peak_player.*
+
+class PeakPlayerFragment : AbsPlayerFragment() {
+
+    private lateinit var playbackControlsFragment: PeakPlayerControlFragment
+    private var lastColor: Int = 0
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_peak_player, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpPlayerToolbar()
+        setUpSubFragments()
+        title.isSelected = true
+        playerImage.setOnClickListener {
+            NavigationUtil.goToLyrics(require
