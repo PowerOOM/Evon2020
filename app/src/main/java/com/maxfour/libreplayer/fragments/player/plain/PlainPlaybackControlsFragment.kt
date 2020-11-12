ce1f9af49b3b3ee3c3cbc1e4fcae0f7d1aa5c481
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-11-12 22:31:28 +0000

    adminlte

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/plain/PlainPlaybackControlsFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/plain/PlainPlaybackControlsFragment.kt
@@ -9,4 +9,26 @@ import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
-im
+import android.widget.SeekBar
+import com.maxfour.appthemehelper.ThemeStore
+import com.maxfour.appthemehelper.util.ATHUtil
+import com.maxfour.appthemehelper.util.ColorUtil
+import com.maxfour.appthemehelper.util.MaterialValueHelper
+import com.maxfour.appthemehelper.util.TintHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.extensions.ripAlpha
+import com.maxfour.libreplayer.fragments.base.AbsPlayerControlsFragment
+import com.maxfour.libreplayer.helper.MusicPlayerRemote
+import com.maxfour.libreplayer.helper.MusicProgressViewUpdateHelper
+import com.maxfour.libreplayer.helper.PlayPauseButtonOnClickHandler
+import com.maxfour.libreplayer.misc.SimpleOnSeekbarChangeListener
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.maxfour.libreplayer.util.ViewUtil
+import kotlinx.android.synthetic.main.fragment_plain_controls_fragment.*
+import kotlinx.android.synthetic.main.media_button.playPauseButton
+import kotlinx.android.synthetic.main.media_button.repeatButton
+import kotlinx.android.synthetic.main.media_button.shuffleButton
+
+class PlainPlaybackControlsFragment : AbsPlayerCont
