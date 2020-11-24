768fb752187fbcaf5004d1e97c170f80a52c254c
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-11-24 18:44:12 +0000

    Some linting

--- app/src/main/java/com/maxfour/libreplayer/fragments/player/tiny/TinyPlayerFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/player/tiny/TinyPlayerFragment.kt
@@ -18,4 +18,49 @@ import com.maxfour.libreplayer.fragments.base.AbsPlayerFragment
 import com.maxfour.libreplayer.fragments.player.PlayerAlbumCoverFragment
 import com.maxfour.libreplayer.helper.MusicPlayerRemote
 import com.maxfour.libreplayer.helper.MusicProgressViewUpdateHelper
-import com.maxfou
+import com.maxfour.libreplayer.helper.PlayPauseButtonOnClickHandler
+import com.maxfour.libreplayer.model.Song
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.maxfour.libreplayer.util.ViewUtil
+import kotlinx.android.synthetic.main.fragment_tiny_player.*
+
+class TinyPlayerFragment : AbsPlayerFragment(), MusicProgressViewUpdateHelper.Callback {
+    override fun onUpdateProgressViews(progress: Int, total: Int) {
+        progressBar.max = total
+
+        val animator = ObjectAnimator.ofInt(progressBar, "progress", progress)
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(animator)
+
+        animatorSet.duration = 1500
+        animatorSet.interpolator = LinearInterpolator()
+        animatorSet.start()
+
+        playerSongTotalTime.text = String.format("%s/%s", MusicUtil.getReadableDurationString(total.toLong()),
+                MusicUtil.getReadableDurationString(progress.toLong()))
+    }
+
+
+    override fun playerToolbar(): Toolbar {
+        return playerToolbar
+    }
+
+    override fun onShow() {
+
+    }
+
+    override fun onHide() {
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
+    }
+
+    override fun toolbarIconColor(): Int {
+        return textColorPrimary
+    }
+
+    private var lastColor: Int = 0
+    o
