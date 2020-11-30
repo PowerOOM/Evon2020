dfbe87a3569024784d8accd0e231adf1004305ba
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-11-30 19:39:01 +0000

    Fixed few bugs

--- app/src/main/java/com/maxfour/libreplayer/fragments/settings/MainSettingsFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/settings/MainSettingsFragment.kt
@@ -16,4 +16,27 @@ class MainSettingsFragment : Fragment(), View.OnClickListener {
         when (v.id) {
             R.id.generalSettings -> inflateFragment(ThemeSettingsFragment(), R.string.general_settings_title)
             R.id.audioSettings -> inflateFragment(AudioSettings(), R.string.pref_header_audio)
-            R.id.nowPlayingSettings -> inflateFragment(NowPlayingSettingsFragment(), R.strin
+            R.id.nowPlayingSettings -> inflateFragment(NowPlayingSettingsFragment(), R.string.now_playing)
+            R.id.personalizeSettings -> inflateFragment(PersonalizeSettingsFragment(), R.string.personalize)
+            R.id.imageSettings -> inflateFragment(ImageSettingFragment(), R.string.pref_header_images)
+            R.id.notificationSettings -> inflateFragment(NotificationSettingsFragment(), R.string.notification)
+            R.id.otherSettings -> inflateFragment(OtherSettingsFragment(), R.string.others)
+            R.id.aboutSettings -> NavigationUtil.goToAbout(requireActivity())
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        generalSettings.setOnClickListener(this)
+        audioSettings.setOnClickListener(this)
+        nowPlayingSettings.setOnClickListener(this)
+        personalizeSettings.setOnClickListener(this)
+        imageSettings.setOnClickListener(this)
+        notificationSettings.setOnClickListener(this)
+        otherSettings.setOnClickListener(this
