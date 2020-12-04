b509c89426c1db5b6be061e6758559da78ae4ec7
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-12-04 22:25:24 +0000

    some tidying

--- app/src/main/java/com/maxfour/libreplayer/fragments/settings/ThemeSettingsFragment.kt
+++ app/src/main/java/com/maxfour/libreplayer/fragments/settings/ThemeSettingsFragment.kt
@@ -23,4 +23,28 @@ class ThemeSettingsFragment : AbsSettingsFragment() {
     override fun invalidateSettings() {
         val generalTheme: Preference? = findPreference("general_theme")
         generalTheme?.let {
-   
+            setSummary(it)
+            it.setOnPreferenceChangeListener { _, newValue ->
+                val theme = newValue as String
+                setSummary(it, newValue)
+                ThemeStore.markChanged(requireContext())
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                    requireActivity().setTheme(PreferenceUtil.getThemeResFromPrefValue(theme))
+                    DynamicShortcutManager(requireContext()).updateDynamicShortcuts()
+                }
+                requireActivity().recreate()
+                true
+            }
+        }
+
+        val accentColorPref: ATEColorPreference = findPreference("accent_color")!!
+        val accentColor = ThemeStore.accentColor(requireContext())
+        accentColorPref.setColor(accentColor, ColorUtil.darkenColor(accentColor))
+
+        accentColorPref.setOnPreferenceClickListener {
+            MaterialDialog(requireActivity(), BottomSheet(LayoutMode.WRAP_CONTENT)).show {
+                cornerRadius(PreferenceUtil.getInstance(requireContext()).dialogCorner)
+                title(R.string.accent_color)
+                positiveButton(R.string.apply)
+                colorChooser(colors = ACCENT_COLORS, allowCustomArgb = true,
