595d9d151c3493fd769777891af0893c0289a50d
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-12-08 18:20:17 +0000

    rdoc

--- app/src/main/java/com/maxfour/libreplayer/glide/ArtistGlideRequest.java
+++ app/src/main/java/com/maxfour/libreplayer/glide/ArtistGlideRequest.java
@@ -29,4 +29,33 @@ public class ArtistGlideRequest {
     private static final DiskCacheStrategy DEFAULT_DISK_CACHE_STRATEGY = DiskCacheStrategy.SOURCE;
     private static final int DEFAULT_ERROR_IMAGE = R.drawable.default_artist_art;
 
-    public static DrawableTypeRequest createBaseRequest(RequestManager requestManager, Artis
+    public static DrawableTypeRequest createBaseRequest(RequestManager requestManager, Artist artist, boolean noCustomImage, boolean forceDownload) {
+        boolean hasCustomImage = CustomArtistImageUtil.Companion.getInstance(App.Companion.getContext()).hasCustomArtistImage(artist);
+        if (noCustomImage || !hasCustomImage) {
+            return requestManager.load(new ArtistImage(artist.getName(), forceDownload));
+        } else {
+            return requestManager.load(CustomArtistImageUtil.getFile(artist));
+        }
+    }
+
+    public static Key createSignature(Artist artist) {
+        return ArtistSignatureUtil.getInstance(App.Companion.getContext()).getArtistSignature(artist.getName());
+    }
+
+    public static class Builder {
+        final RequestManager requestManager;
+        final Artist artist;
+        boolean noCustomImage;
+        boolean forceDownload;
+
+        private Builder(@NonNull RequestManager requestManager, Artist artist) {
+            this.requestManager = requestManager;
+            this.artist = artist;
+        }
+
+        public static Builder from(@NonNull RequestManager requestManager, Artist artist) {
+            return new Builder(requestManager, artist);
+        }
+
+        public PaletteBuilder generatePalette(Context context) {
+            return new Pa
