1e44d3f3680c1478cd6aa4855d90a54e6d0de3a2
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-12-19 12:06:49 +0000

    Run test for travis-ci/apt-package-whitelist#4121 in dist trusty. (ninja-build)

--- app/src/main/java/com/maxfour/libreplayer/glide/artistimage/ArtistImageLoader.kt
+++ app/src/main/java/com/maxfour/libreplayer/glide/artistimage/ArtistImageLoader.kt
@@ -6,4 +6,44 @@ import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.data.DataFetcher
 import com.bumptech.glide.load.model.GenericLoaderFactory
 import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide
+import com.bumptech.glide.load.model.ModelLoader
+import com.bumptech.glide.load.model.ModelLoaderFactory
+import com.bumptech.glide.load.model.stream.StreamModelLoader
+import com.maxfour.libreplayer.deezer.Data
+import com.maxfour.libreplayer.deezer.DeezerApiService
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+import okhttp3.OkHttpClient
+import java.io.IOException
+import java.io.InputStream
+import java.util.concurrent.TimeUnit
+
+class ArtistImage(val artistName: String, val skipOkHttpCache: Boolean)
+
+class ArtistImageFetcher(
+        private val context: Context,
+        private val deezerApiService: DeezerApiService,
+        val model: ArtistImage,
+        val urlLoader: ModelLoader<GlideUrl, InputStream>,
+        val width: Int,
+        val height: Int
+) : DataFetcher<InputStream> {
+
+    private var urlFetcher: DataFetcher<InputStream>? = null
+    private var isCancelled: Boolean = false
+
+    override fun cleanup() {
+        urlFetcher?.cleanup()
+    }
+
+    override fun getId(): String {
+        return model.artistName
+    }
+
+    override fun cancel() {
+        isCancelled = true
+        urlFetcher?.cancel()
+    }
+
+    override fun loadData(priority: Priority?): InputStream? {
+        if (!Musi
