767ce92177e2ff66a4f46bca3c3ac82c2da74732
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2020-12-29 11:50:22 +0000

    #585

--- app/src/main/java/com/maxfour/libreplayer/helper/menu/PlaylistMenuHelper.kt
+++ app/src/main/java/com/maxfour/libreplayer/helper/menu/PlaylistMenuHelper.kt
@@ -15,4 +15,33 @@ import com.maxfour.libreplayer.helper.MusicPlayerRemote
 import com.maxfour.libreplayer.loaders.PlaylistSongsLoader
 import com.maxfour.libreplayer.misc.WeakContextAsyncTask
 import com.maxfour.libreplayer.model.AbsCustomPlaylist
-import com.maxfour.libreplayer
+import com.maxfour.libreplayer.model.Playlist
+import com.maxfour.libreplayer.model.Song
+import com.maxfour.libreplayer.util.PlaylistsUtil
+import java.util.*
+
+object PlaylistMenuHelper {
+
+    fun handleMenuClick(activity: AppCompatActivity,
+                        playlist: Playlist, item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_play -> {
+                MusicPlayerRemote.openQueue(getPlaylistSongs(activity, playlist), 9, true)
+                return true
+            }
+            R.id.action_play_next -> {
+                MusicPlayerRemote.playNext(getPlaylistSongs(activity, playlist))
+                return true
+            }
+            R.id.action_add_to_playlist -> {
+                AddToPlaylistDialog.create(getPlaylistSongs(activity, playlist))
+                        .show(activity.supportFragmentManager, "ADD_PLAYLIST")
+                return true
+            }
+            R.id.action_add_to_current_playing -> {
+                MusicPlayerRemote.enqueue(getPlaylistSongs(activity, playlist))
+                return true
+            }
+            R.id.action_rename_playlist -> {
+                RenamePlaylistDialog.create(playlist.id.toLong())
+                        .show(activity.supportFragmen
