e40a93e66f33302e1b30bf31adcce3cc2957b320
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-01-02 19:48:16 +0000

    unused code

--- app/src/main/java/com/maxfour/libreplayer/loaders/AlbumLoader.kt
+++ app/src/main/java/com/maxfour/libreplayer/loaders/AlbumLoader.kt
@@ -12,4 +12,43 @@ object AlbumLoader {
 
     fun getAlbums(
             context: Context,
-            query:
+            query: String
+    ): ArrayList<Album> {
+        val songs = SongLoader.getSongs(SongLoader.makeSongCursor(
+                context,
+                AudioColumns.ALBUM + " LIKE ?",
+                arrayOf("%$query%"),
+                getSongLoaderSortOrder(context))
+        )
+        return splitIntoAlbums(songs)
+    }
+
+    fun getAlbum(
+            context: Context,
+            albumId: Int
+    ): Album {
+        val songs = SongLoader.getSongs(
+                SongLoader.makeSongCursor(
+                        context,
+                        AudioColumns.ALBUM_ID + "=?",
+                        arrayOf(albumId.toString()),
+                        getSongLoaderSortOrder(context)))
+        val album = Album(songs)
+        sortSongsByTrackNumber(album)
+        return album
+    }
+
+    fun getAllAlbums(
+            context: Context
+    ): ArrayList<Album> {
+        val songs = SongLoader.getSongs(SongLoader.makeSongCursor(context, null, null, getSongLoaderSortOrder(context)))
+        return splitIntoAlbums(songs)
+    }
+
+    fun splitIntoAlbums(
+            songs: ArrayList<Song>?
+    ): ArrayList<Album> {
+        val albums = ArrayList<Album>()
+        if (songs != null) {
+            for (song in songs) {
+                getOrCreateAlbum(albums, song
