47807e942b228073a6978bf339c7ab58e30192ff
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-01-03 12:31:18 +0000

    Added a test.

--- app/src/main/java/com/maxfour/libreplayer/loaders/PlaylistLoader.kt
+++ app/src/main/java/com/maxfour/libreplayer/loaders/PlaylistLoader.kt
@@ -1,4 +1,48 @@
 package com.maxfour.libreplayer.loaders
 
 import android.content.Context
-import android.databa
+import android.database.Cursor
+import android.provider.BaseColumns
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.PlaylistsColumns
+import com.maxfour.libreplayer.model.Playlist
+import io.reactivex.Observable
+import java.util.*
+
+object PlaylistLoader {
+
+    private fun getPlaylistFlowable(
+            cursor: Cursor?
+    ): Observable<Playlist> {
+        return Observable.create { e ->
+            var playlist = Playlist()
+
+            if (cursor != null && cursor.moveToFirst()) {
+                playlist = getPlaylistFromCursorImpl(cursor)
+            }
+            cursor?.close()
+
+            e.onNext(playlist)
+            e.onComplete()
+        }
+    }
+
+    fun getPlaylist(
+            cursor: Cursor?
+    ): Playlist {
+        var playlist = Playlist()
+
+        if (cursor != null && cursor.moveToFirst()) {
+            playlist = getPlaylistFromCursorImpl(cursor)
+        }
+        cursor?.close()
+        return playlist
+    }
+
+    fun getPlaylistFlowable(
+            context: Context,
+            playlistName: String
+    ): Observable<Playlist> {
+        return getPlaylistFlowable(makePlaylistCursor(
+                context,
+                PlaylistsColumns
