217e038a72c115d57b6ae8454fbb18d371096c65
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-02-22 06:15:38 +0000

    Typographical corrections and changes

--- app/src/main/java/com/maxfour/libreplayer/misc/AsyncSearchResultLoader.kt
+++ app/src/main/java/com/maxfour/libreplayer/misc/AsyncSearchResultLoader.kt
@@ -5,4 +5,32 @@ import android.text.TextUtils
 import com.maxfour.libreplayer.R
 import com.maxfour.libreplayer.loaders.AlbumLoader
 import com.maxfour.libreplayer.loaders.ArtistLoader
-imp
+import com.maxfour.libreplayer.loaders.SongLoader
+import java.util.*
+
+internal class AsyncSearchResultLoader(context: Context, private val query: String) : WrappedAsyncTaskLoader<List<Any>>(context) {
+
+    override fun loadInBackground(): List<Any>? {
+        val results = ArrayList<Any>()
+        if (!TextUtils.isEmpty(query)) {
+            val songs = SongLoader.getSongs(context, query.trim { it <= ' ' })
+            if (!songs.isEmpty()) {
+                results.add(context.resources.getString(R.string.songs))
+                results.addAll(songs)
+            }
+
+            val artists = ArtistLoader.getArtists(context, query.trim { it <= ' ' })
+            if (!artists.isEmpty()) {
+                results.add(context.resources.getString(R.string.artists))
+                results.addAll(artists)
+            }
+
+            val albums = AlbumLoader.getAlbums(context, query.trim { it <= ' ' })
+            if (!albums.isEmpty()) {
+                results.add(context.resources.getString(R.string.albums))
+                results.addAll(albums)
+            }
+        }
+        return results
+    }
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/misc/CustomFragmentStatePagerAdapter.java
@@ -0,0 +1,5 @@
+package com.maxfour.libreplayer.misc;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import and
