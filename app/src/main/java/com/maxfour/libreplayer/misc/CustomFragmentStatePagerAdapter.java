8cf37850a8e84057f85081e6df2260895e8fc025
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-02-22 12:09:30 +0000

    Workflow update

--- app/src/main/java/com/maxfour/libreplayer/misc/CustomFragmentStatePagerAdapter.java
+++ app/src/main/java/com/maxfour/libreplayer/misc/CustomFragmentStatePagerAdapter.java
@@ -2,4 +2,34 @@ package com.maxfour.libreplayer.misc;
 
 import android.os.Bundle;
 import android.os.Parcelable;
-import and
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Implementation of {@link PagerAdapter} that
+ * uses a {@link Fragment} to manage each page. This class also handles
+ * saving and restoring of fragment's state.
+ * <p/>
+ * <p>This version of the pager is more useful when there are a large number
+ * of pages, working more like a list view.  When pages are not visible to
+ * the user, their entire fragment may be destroyed, only keeping the saved
+ * state of that fragment.  This allows the pager to hold on to much less
+ * memory associated with each visited page as compared to
+ * {@link FragmentPagerAdapter} at the cost of potentially more overhead when
+ * switching between pages.
+ * <p/>
+ * <p>When using FragmentPagerAdapter the host ViewPager must have a
+ * valid ID set.</p>
+ * <p/>
+ * <p>Subclasses only need to implement {@link #getItem(int)}
+ * and {@link #getCount()} to have a wo
