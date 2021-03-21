e4a7c83c5ba854dfdf1cfe2bf6e6fb80a9c92b4d
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-03-21 13:30:40 +0000

    add ui framework document

--- app/src/main/java/com/maxfour/libreplayer/misc/CustomFragmentStatePagerAdapter.java
+++ app/src/main/java/com/maxfour/libreplayer/misc/CustomFragmentStatePagerAdapter.java
@@ -171,4 +171,37 @@ public abstract class CustomFragmentStatePagerAdapter extends PagerAdapter {
         }
         for (int i = 0; i < mFragments.size(); i++) {
             Fragment f = mFragments.get(i);
-            if (f != 
+            if (f != null && f.isAdded()) {
+                if (state == null) {
+                    state = new Bundle();
+                }
+                String key = "f" + i;
+                mFragmentManager.putFragment(state, key, f);
+            }
+        }
+        return state;
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+        if (state != null) {
+            Bundle bundle = (Bundle) state;
+            bundle.setClassLoader(loader);
+            Parcelable[] fss = bundle.getParcelableArray("states");
+            mSavedState.clear();
+            mFragments.clear();
+            if (fss != null) {
+                for (int i = 0; i < fss.length; i++) {
+                    mSavedState.add((Fragment.SavedState) fss[i]);
+                }
+            }
+            Iterable<String> keys = bundle.keySet();
+            for (String key : keys) {
+                if (key.startsWith("f")) {
+                    int index = Integer.parseInt(key.substring(1));
+                    Fragment f = mFragmentManager.getFragment(bundle, key);
+                    if (f != null) {
+                        while (mFragments.size() <= index) {
+                            mFragments.add(null);
+                        }
+                        f.setMenuVisibi
