f0414dfcf1880cabc1923dffbd739c2396bd7d00
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-04-20 11:43:11 +0000

    Release 4.1.1

--- app/src/main/java/com/maxfour/libreplayer/model/smartplaylist/HistoryPlaylist.java
+++ app/src/main/java/com/maxfour/libreplayer/model/smartplaylist/HistoryPlaylist.java
@@ -17,4 +17,36 @@ import java.util.ArrayList;
 public class HistoryPlaylist extends AbsSmartPlaylist {
 
     public static final Creator<HistoryPlaylist> CREATOR = new Creator<HistoryPlaylist>() {
-        public HistoryPlaylist createFromParcel(Parcel so
+        public HistoryPlaylist createFromParcel(Parcel source) {
+            return new HistoryPlaylist(source);
+        }
+
+        public HistoryPlaylist[] newArray(int size) {
+            return new HistoryPlaylist[size];
+        }
+    };
+
+    public HistoryPlaylist(@NonNull Context context) {
+        super(context.getString(R.string.history), R.drawable.ic_access_time_white_24dp);
+    }
+
+    protected HistoryPlaylist(Parcel in) {
+        super(in);
+    }
+
+    @NonNull
+    @Override
+    public ArrayList<Song> getSongs(@NotNull @NonNull Context context) {
+        return TopAndRecentlyPlayedSongsLoader.INSTANCE.getRecentlyPlayedSongs(context);
+    }
+
+    @Override
+    public void clear(@NonNull Context context) {
+        HistoryStore.getInstance(context).clear();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/model/smartplaylist/ShuffleAllPlaylist.java
@@ -0,0 +1,10 @@
+package com.maxfour.libreplayer.model.smartplaylist;
+
+import android.content.Context;
+import android.os.Parcel;
+
+import androidx.annotation.NonNull;
+
+import com.maxfour.libreplayer.R;
+import com.maxfour.libreplayer.loaders.SongLoader;
+import com.maxfour.libreplayer.
