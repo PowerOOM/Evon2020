7089e01818783c1ac798b17c3cef757d6ff551a0
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-05-04 05:30:30 +0000

    add new dist

--- app/src/main/java/com/maxfour/libreplayer/mvp/presenter/ArtistDetailsPresenter.kt
+++ app/src/main/java/com/maxfour/libreplayer/mvp/presenter/ArtistDetailsPresenter.kt
@@ -12,4 +12,40 @@ import java.util.Locale
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-interfa
+interface ArtistDetailsView : BaseView {
+    fun artist(artist: Artist)
+    fun artistInfo(lastFmArtist: LastFmArtist?)
+    fun complete()
+}
+
+interface ArtistDetailsPresenter : Presenter<ArtistDetailsView> {
+
+    fun loadArtist(artistId: Int)
+
+    fun loadBiography(
+            name: String, lang: String? = Locale.getDefault().language, cache: String?
+    )
+
+    class ArtistDetailsPresenterImpl @Inject constructor(
+            private val repository: Repository
+    ) : PresenterImpl<ArtistDetailsView>(), ArtistDetailsPresenter, CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.IO + job
+
+        private val job = Job()
+
+        override fun loadBiography(name: String, lang: String?, cache: String?) {
+            launch {
+                when (val result = repository.artistInfo(name, lang, cache)) {
+                    is Result.Success -> withContext(Dispatchers.Main) {
+                        view?.artistInfo(result.data)
+                    }
+                    is Result.Error -> withContext(Dispatchers.Main) {
+                    }
+                }
+            }
+        }
+
+        override fun loadArtist(artistId: Int) {
+            launch {
+              
