f4f38107a6bbb8d903dfb8b80efe9a26dc6b60fc
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-05-09 06:51:42 +0000

    release 0.9

--- app/src/main/java/com/maxfour/libreplayer/mvp/presenter/HomePresenter.kt
+++ app/src/main/java/com/maxfour/libreplayer/mvp/presenter/HomePresenter.kt
@@ -12,4 +12,43 @@ import kotlinx.coroutines.*
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-operator 
+operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    add(disposable)
+}
+
+interface HomeView : BaseView {
+    fun sections(sections: ArrayList<Home>)
+}
+
+interface HomePresenter : Presenter<HomeView> {
+    fun loadSections()
+
+    class HomePresenterImpl @Inject constructor(
+            private val repository: Repository
+    ) : PresenterImpl<HomeView>(), HomePresenter, CoroutineScope {
+        private val job = Job()
+
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.IO + job
+
+        override fun detachView() {
+            super.detachView()
+            job.cancel()
+        }
+
+        override fun loadSections() {
+            launch {
+                val list = ArrayList<Home>()
+                val recentArtistResult = listOf(
+                        repository.topArtists(),
+                        repository.topAlbums(),
+                        repository.recentArtists(),
+                        repository.recentAlbums(),
+                        repository.favoritePlaylist()
+                )
+                for (r in recentArtistResult) {
+                    when (r) {
+                        is Result.Success -> list.add(r.data)
+                    }
+                }
+     
