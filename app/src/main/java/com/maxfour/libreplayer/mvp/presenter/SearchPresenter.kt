6e82d769a18e325d653a5856ba10902e2966d7d5
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-05-27 17:32:54 +0000

    Compatibility with latest update.

--- app/src/main/java/com/maxfour/libreplayer/mvp/presenter/SearchPresenter.kt
+++ app/src/main/java/com/maxfour/libreplayer/mvp/presenter/SearchPresenter.kt
@@ -19,4 +19,26 @@ interface SearchPresenter : Presenter<SearchView> {
     fun search(query: String?)
 
     class SearchPresenterImpl @Inject constructor(
-            private val
+            private val repository: Repository
+    ) : PresenterImpl<SearchView>(), SearchPresenter, CoroutineScope {
+
+        override val coroutineContext: CoroutineContext
+            get() = Dispatchers.IO + job
+
+        private var job: Job = Job()
+
+        override fun detachView() {
+            super.detachView()
+            job.cancel()
+        }
+
+        override fun search(query: String?) {
+            launch {
+                when (val result = repository.search(query)) {
+                    is Success -> withContext(Dispatchers.Main) { view?.showData(result.data) }
+                    is Error -> withContext(Dispatchers.Main) { view?.showEmptyView() }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/preferences/BlacklistPreferenceDialog.kt
@@ -0,0 +1,12 @@
+package com.maxfour.libreplayer.preferences
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.text.Html
+import android.util.AttributeSet
+import androidx.fragment.app.DialogFragment
+import com.afollestad.materialdialogs.LayoutMode
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materia
