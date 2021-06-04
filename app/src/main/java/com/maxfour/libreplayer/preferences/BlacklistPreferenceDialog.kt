be999c98852213192e73d4523c083d78e7f2008d
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-06-04 21:55:16 +0000

    Update master.js

--- app/src/main/java/com/maxfour/libreplayer/preferences/BlacklistPreferenceDialog.kt
+++ app/src/main/java/com/maxfour/libreplayer/preferences/BlacklistPreferenceDialog.kt
@@ -9,4 +9,32 @@ import android.util.AttributeSet
 import androidx.fragment.app.DialogFragment
 import com.afollestad.materialdialogs.LayoutMode
 import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materia
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.list.listItems
+import com.maxfour.appthemehelper.ThemeStore
+import com.maxfour.appthemehelper.common.prefs.supportv7.ATEDialogPreference
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.dialogs.BlacklistFolderChooserDialog
+import com.maxfour.libreplayer.providers.BlacklistStore
+import com.maxfour.libreplayer.util.PreferenceUtil
+import java.io.File
+import java.util.*
+
+class BlacklistPreference : ATEDialogPreference {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        icon?.setColorFilter(ThemeStore.textColorSecondary(context), PorterDuff.Mode.SRC_IN)
+    }
+}
+
+class BlacklistPreferenceDialog : DialogFragment(), BlacklistFolderChooserDialog.FolderCallback {
+    companion object {
+        fun newInstance(): BlacklistPreferenceDialog {
+            return BlacklistPreferen
