db80b32e744c62644512ee800036706bc62f7ec2
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-07-26 05:50:12 +0000

    Linux 3.0.94

--- app/src/main/java/com/maxfour/libreplayer/providers/MusicPlaybackQueueStore.java
+++ app/src/main/java/com/maxfour/libreplayer/providers/MusicPlaybackQueueStore.java
@@ -18,4 +18,37 @@ import java.util.ArrayList;
 
 import io.reactivex.Observable;
 
-public class Mu
+public class MusicPlaybackQueueStore extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "music_playback_state.db";
+    public static final String PLAYING_QUEUE_TABLE_NAME = "playing_queue";
+    public static final String ORIGINAL_PLAYING_QUEUE_TABLE_NAME = "original_playing_queue";
+    private static final int VERSION = 10;
+    @Nullable
+    private static MusicPlaybackQueueStore sInstance = null;
+
+    /**
+     * Constructor of <code>MusicPlaybackState</code>
+     *
+     * @param context The {@link Context} to use
+     */
+    public MusicPlaybackQueueStore(final @NonNull Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    /**
+     * @param context The {@link Context} to use
+     * @return A new instance of this class.
+     */
+    @NonNull
+    public static synchronized MusicPlaybackQueueStore getInstance(@NonNull final Context context) {
+        if (sInstance == null) {
+            sInstance = new MusicPlaybackQueueStore(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate(@NonNull final SQLiteDatabase db) {
+        createTable(db, PLAYING_QUEUE_TABLE_NAME);
+        createTable(db, ORIGINAL_PLAYING_QUEUE_TABLE_NAME);
+    
