9d37e25501d5cde4d5b12af40f2270385c0f2219
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-08-04 11:49:30 +0000

    fixes #73

--- app/src/main/java/com/maxfour/libreplayer/service/MediaSessionCallback.kt
+++ app/src/main/java/com/maxfour/libreplayer/service/MediaSessionCallback.kt
@@ -11,4 +11,51 @@ import com.maxfour.libreplayer.service.MusicService.*
 import com.maxfour.libreplayer.util.MusicUtil
 import java.util.*
 
-class MediaSessionCallback(private val context: Contex
+class MediaSessionCallback(private val context: Context,
+                           private val musicService: MusicService
+) : MediaSessionCompat.Callback() {
+
+    override fun onPlay() {
+        super.onPlay()
+        musicService.play()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        musicService.pause()
+    }
+
+    override fun onSkipToNext() {
+        super.onSkipToNext()
+        musicService.playNextSong(true)
+    }
+
+    override fun onSkipToPrevious() {
+        super.onSkipToPrevious()
+        musicService.back(true)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        musicService.quit()
+    }
+
+    override fun onSeekTo(pos: Long) {
+        super.onSeekTo(pos)
+        musicService.seek(pos.toInt())
+    }
+
+    override fun onMediaButtonEvent(mediaButtonIntent: Intent): Boolean {
+        return MediaButtonIntentReceiver.handleIntent(context, mediaButtonIntent)
+    }
+
+    override fun onCustomAction(action: String, extras: Bundle?) {
+        when (action) {
+            CYCLE_REPEAT -> {
+                cycleRepeatMode()
+                musicService.updateMediaSessionPlaybackState()
+            }
+
+            TOGGLE_SHUFFLE -> {
+                musicService.toggleShuffle()
+                musicService.updateMediaSessionPla
