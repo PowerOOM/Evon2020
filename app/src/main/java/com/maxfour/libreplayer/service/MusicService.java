641a001e6440486a652f7704990b97c0b50e2cad
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-08-05 14:38:07 +0000

    Lots of work

--- app/src/main/java/com/maxfour/libreplayer/service/MusicService.java
+++ app/src/main/java/com/maxfour/libreplayer/service/MusicService.java
@@ -1,4 +1,39 @@
 package com.maxfour.libreplayer.service;
 
 import android.app.PendingIntent;
-imp
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.audiofx.AudioEffect;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.Process;
+import android.provider.MediaStore;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
+
+import com.bumptech.glide.BitmapRequestBuilder;
+im
