04f97617dcbc2f0f3f20a64138133fba17793ee2
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-09-25 15:53:53 +0000

    Rebranding

--- app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationImpl.kt
+++ app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationImpl.kt
@@ -1,3 +1,31 @@
 package com.maxfour.libreplayer.service.notification
 
-import android.app.Pe
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.text.Html
+import androidx.core.app.NotificationCompat
+import androidx.media.app.NotificationCompat.MediaStyle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.activities.MainActivity
+import com.maxfour.libreplayer.glide.SongGlideRequest
+import com.maxfour.libreplayer.glide.palette.BitmapPaletteWrapper
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.service.MusicService.*
+import com.maxfour.libreplayer.util.MusicUtil
+import com.maxfour.libreplayer.util.PlayerColorUtil
+import com.maxfour.libreplayer.util.PreferenceUtil
+
+class PlayingNotificationImpl : PlayingNotification() {
+    private var target: Target<BitmapPaletteWrapper>? = null
+    @Synchronized
+    override fun update() {
