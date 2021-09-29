2c7c68cbd6398564dac4c268886d135068748634
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-09-29 14:05:37 +0000

    Fix whitespaces.

--- app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationOreo.kt
+++ app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationOreo.kt
@@ -33,4 +33,30 @@ class PlayingNotificationOreo : PlayingNotification() {
     private var target: Target<BitmapPaletteWrapper>? = null
 
     private fun getCombinedRemoteViews(collapsed: Boolean, song: Song): RemoteViews {
-        val remoteViews = RemoteViews(service.pac
+        val remoteViews = RemoteViews(service.packageName, if (collapsed) R.layout.layout_notification_collapsed else R.layout.layout_notification_expanded)
+        remoteViews.setTextViewText(R.id.appName, service.getString(R.string.app_name) + " • " + song.albumName)
+        remoteViews.setTextViewText(R.id.title, song.title)
+        remoteViews.setTextViewText(R.id.subtitle, song.artistName)
+        linkButtons(remoteViews)
+        return remoteViews
+    }
+
+    override fun update() {
+        stopped = false
+        val song = service.currentSong
+        val isPlaying = service.isPlaying
+
+        val notificationLayout = getCombinedRemoteViews(true, song)
+        val notificationLayoutBig = getCombinedRemoteViews(false, song)
+
+        val action = Intent(service, MainActivity::class.java)
+        action.putExtra("expand", true)
+        action.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+        val clickIntent = PendingIntent
+                .getActivity(service, 0, action, PendingIntent.FLAG_UPDATE_CURRENT)
+        val deleteIntent = buildPendingIntent(service, ACTION_QUIT, null)
+
+        val builder = NotificationCompat.Builder(service, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentInten
