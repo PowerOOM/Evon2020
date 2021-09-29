4b735fb2e09ee1c160ad601979cd79cc8c33e490
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-09-29 06:18:57 +0000

    "master"

--- app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationOreo.kt
+++ app/src/main/java/com/maxfour/libreplayer/service/notification/PlayingNotificationOreo.kt
@@ -10,4 +10,27 @@ import android.graphics.drawable.Drawable
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.bumptech.glide.Glide
-i
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
+import com.maxfour.appthemehelper.util.ATHUtil
+import com.maxfour.appthemehelper.util.ColorUtil
+import com.maxfour.appthemehelper.util.MaterialValueHelper
+import com.maxfour.libreplayer.R
+import com.maxfour.libreplayer.activities.MainActivity
+import com.maxfour.libreplayer.glide.SongGlideRequest
+import com.maxfour.libreplayer.glide.palette.BitmapPaletteWrapper
+import com.maxfour.libreplayer.model.Song
+import com.maxfour.libreplayer.service.MusicService
+import com.maxfour.libreplayer.service.MusicService.*
+import com.maxfour.libreplayer.util.PlayerUtil
+import com.maxfour.libreplayer.util.PlayerUtil.createBitmap
+import com.maxfour.libreplayer.util.PreferenceUtil
+import com.maxfour.libreplayer.util.color.MediaNotificationProcessor
+
+class PlayingNotificationOreo : PlayingNotification() {
+
+    private var target: Target<BitmapPaletteWrapper>? = null
+
+    private fun getCombinedRemoteViews(collapsed: Boolean, song: Song): RemoteViews {
+        val remoteViews = RemoteViews(service.pac
