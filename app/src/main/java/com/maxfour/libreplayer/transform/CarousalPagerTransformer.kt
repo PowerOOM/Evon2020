859ac3f5f382bb208369d7b0d4b9136d2de6acd0
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-10-08 07:42:17 +0000

    Updates todo

--- app/src/main/java/com/maxfour/libreplayer/transform/CarousalPagerTransformer.kt
+++ app/src/main/java/com/maxfour/libreplayer/transform/CarousalPagerTransformer.kt
@@ -25,4 +25,15 @@ class CarousalPagerTransformer(context: Context) : ViewPager.PageTransformer {
         val offsetRate = offsetX.toFloat() * 0.30f / viewPager!!.measuredWidth
         val scaleFactor = 1 - abs(offsetRate)
         if (scaleFactor > 0) {
-    
+            view.scaleX = scaleFactor
+            view.scaleY = scaleFactor
+            view.translationX = -maxTranslateOffsetX * offsetRate
+        }
+    }
+
+    private fun dp2px(context: Context, dipValue: Float): Int {
+        val m = context.resources.displayMetrics.density
+        return (dipValue * m + 0.5f).toInt()
+    }
+
+}
---
+++ app/src/main/java/com/maxfour/libreplayer/transform/CascadingPageTransformer.kt
@@ -0,0 +1,28 @@
+package com.maxfour.libreplayer.transform
+
+import android.annotation.SuppressLint
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+
+class CascadingPageTransformer : ViewPager.PageTransformer {
+
+    private var mScaleOffset = 40
+
+    fun setScaleOffset(mScaleOffset: Int) {
+        this.mScaleOffset = mScaleOffset
+    }
+
+    @SuppressLint("NewApi")
+    override fun transformPage(page: View, position: Float) {
+        if (position <= 0.0f) {
+            page.translationX = 0f
+            page.rotation = 45 * position
+            page.translationX = page.width / 3 * position
+        } else if (position <= 1f) {
+            val scale = (page.width - mScaleOffset * position) / page.width.toFloat()
+
+            page.scaleX = scale
+            page.scaleY = scale
+
+            page.translationX = -page.width * position
+            page.translation
