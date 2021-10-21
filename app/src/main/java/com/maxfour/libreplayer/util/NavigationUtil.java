22f6b74fbd7ebbbf5c1574a514aa1bad46761051
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-10-21 09:07:49 +0000

    Add LICENSE and README

--- app/src/main/java/com/maxfour/libreplayer/util/NavigationUtil.java
+++ app/src/main/java/com/maxfour/libreplayer/util/NavigationUtil.java
@@ -102,4 +102,26 @@ public class NavigationUtil {
         ActivityCompat.startActivity(activity, new Intent(activity, SettingsActivity.class), null);
     }
 
-   
+    public static void goToUserInfo(@NonNull Activity activity,
+                                    @NonNull ActivityOptions activityOptions) {
+        ActivityCompat.startActivity(activity, new Intent(activity, UserInfoActivity.class),
+                activityOptions.toBundle());
+    }
+
+    public static void openEqualizer(@NonNull final Activity activity) {
+        stockEqalizer(activity);
+    }
+
+    private static void stockEqalizer(@NonNull Activity activity) {
+        final int sessionId = MusicPlayerRemote.INSTANCE.getAudioSessionId();
+        if (sessionId == AudioEffect.ERROR_BAD_VALUE) {
+            Toast.makeText(activity, activity.getResources().getString(R.string.no_audio_ID),
+                    Toast.LENGTH_LONG).show();
+        } else {
+            try {
+                final Intent effects = new Intent(AudioEffect.ACTION_DISPLAY_AUDIO_EFFECT_CONTROL_PANEL);
+                effects.putExtra(AudioEffect.EXTRA_AUDIO_SESSION, sessionId);
+                effects.putExtra(AudioEffect.EXTRA_CONTENT_TYPE, AudioEffect.CONTENT_TYPE_MUSIC);
+                activity.startActivityForResult(effects, 0);
+            } catch (@NonNull final ActivityNotFoundException notFound) {
+                Toast.makeText(activity, activity.getResource
