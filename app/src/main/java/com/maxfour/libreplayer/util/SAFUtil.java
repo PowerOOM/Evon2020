aa3698523f5095f1e4e4ea0ab0ff43a26e7cb7cb
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-10-27 08:23:19 +0000

    Folder restructure

--- app/src/main/java/com/maxfour/libreplayer/util/SAFUtil.java
+++ app/src/main/java/com/maxfour/libreplayer/util/SAFUtil.java
@@ -4,4 +4,43 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.conte
+import android.content.UriPermission;
+import android.net.Uri;
+import android.os.Build;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.documentfile.provider.DocumentFile;
+import androidx.fragment.app.Fragment;
+
+import com.maxfour.libreplayer.R;
+import com.maxfour.libreplayer.model.Song;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
+import org.jaudiotagger.audio.generic.Utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SAFUtil {
+
+    public static final String TAG = SAFUtil.class.getSimpleName();
+    public static final String SEPARATOR = "###/SAF/###";
+
+    public static final int REQUEST_SAF_PICK_FILE = 42;
+    public static final int REQUEST_SAF_PICK_TREE = 43;
+
+    public static boolean isSAFRequired(File file) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && !file.canWrite();
+    }
+
+    public static boolean isSAFRequire
