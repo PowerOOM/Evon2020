640d38ecd6cf5a7f2477f03e3c1241db675c5a18
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-11-12 09:29:45 +0000

    fix #62

---
+++ app/src/main/java/com/maxfour/libreplayer/views/LollipopFixedWebView.java
@@ -0,0 +1,19 @@
+package com.maxfour.libreplayer.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+public class LollipopFixedWebView extends WebView {
+    public LollipopFixedWebView(Context context) {
+        super(getFixedContext(context));
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs) {
+        super(getFixedContext(context), attrs);
+    }
+
+    public Lollipop
