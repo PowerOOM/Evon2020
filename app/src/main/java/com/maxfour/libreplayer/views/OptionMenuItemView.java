352032641ebee70f2069e3dbe76b94dc0e4e826a
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2021-11-19 08:06:22 +0000

    fix template

--- app/src/main/java/com/maxfour/libreplayer/views/OptionMenuItemView.java
+++ app/src/main/java/com/maxfour/libreplayer/views/OptionMenuItemView.java
@@ -13,4 +13,40 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.core.content.ContextCompat;
 
-import com.maxfour.appth
+import com.maxfour.appthemehelper.ThemeStore;
+import com.maxfour.appthemehelper.util.ColorUtil;
+import com.maxfour.libreplayer.R;
+
+public class OptionMenuItemView extends FrameLayout {
+
+    TextView textView;
+    AppCompatImageView iconImageView;
+
+    public OptionMenuItemView(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public OptionMenuItemView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public OptionMenuItemView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, -1);
+    }
+
+    public OptionMenuItemView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        setBackground(ContextCompat.getDrawable(context, R.drawable.option_menu_background));
+
+        setClickable(true);
+        setFocusable(true);
+
+        inflate(context, R.layout.item_option_menu, this);
+
+        textView = findViewById(R.id.title);
+        iconImageView = findViewById(R.id.icon);
+
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.OptionMenuItemView, 0, 0);
+
+        String titl
