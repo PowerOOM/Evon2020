7eff8a6efffec4610ad3fadaecb14891f42e0e00
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2023-02-13 17:10:11 +0000

    update to 0.1.2

---
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/ThemeStore.kt
@@ -0,0 +1,8 @@
+package com.maxfour.appthemehelper
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import androidx.annotation.*
+import androidx.annotatio
