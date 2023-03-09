cf2742e7ef20e36e965d143a5beec27bd3744583
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2023-03-09 21:11:23 +0000

    Fixes #32

--- appthemehelper/src/main/java/com/maxfour/appthemehelper/common/prefs/supportv7/ATEPreference.kt
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/common/prefs/supportv7/ATEPreference.kt
@@ -8,4 +8,13 @@ import com.maxfour.appthemehelper.ThemeStore
 
 class ATEPreference @JvmOverloads constructor(
         context: Context,
-        attrs: At
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0
+) : Preference(context, attrs, defStyleAttr, defStyleRes) {
+
+    init {
+
+        icon?.setColorFilter(ThemeStore.textColorSecondary(context), PorterDuff.Mode.SRC_IN)
+    }
+}
---
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/common/prefs/supportv7/ATEPreferenceFragmentCompat.kt
@@ -0,0 +1,6 @@
+package com.maxfour.appthemehelper.common.prefs.supportv7
+
+
+import androidx.fragment.app.DialogFragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCom
