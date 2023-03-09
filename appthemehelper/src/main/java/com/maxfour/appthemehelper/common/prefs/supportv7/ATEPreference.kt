7b08e2adc098d994f1e665f50fa0a7960510aeaf
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2023-03-09 15:32:17 +0000

    eCryptfs: Fix lockdep warning in miscdev operations

---
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/common/prefs/supportv7/ATEPreference.kt
@@ -0,0 +1,11 @@
+package com.maxfour.appthemehelper.common.prefs.supportv7
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.util.AttributeSet
+import androidx.preference.Preference
+import com.maxfour.appthemehelper.ThemeStore
+
+class ATEPreference @JvmOverloads constructor(
+        context: Context,
+        attrs: At
