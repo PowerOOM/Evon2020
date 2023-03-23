29e6c307e71fc3244f7dc882e888c449838f0b11
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2023-03-23 13:31:07 +0000

    remove unneeded line

--- appthemehelper/src/main/java/com/maxfour/appthemehelper/util/NavigationViewUtil.kt
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/util/NavigationViewUtil.kt
@@ -9,4 +9,24 @@ import com.maxfour.appthemehelper.ThemeStore
 object NavigationViewUtil {
 
     fun setItemIconColors(navigationView: NavigationView, @ColorInt normalColor: Int, @ColorInt selectedColor: Int) {
-        val iconSl = ColorStateList(arrayOf(intArrayOf(-android.R.a
+        val iconSl = ColorStateList(arrayOf(intArrayOf(-android.R.attr.state_checked), intArrayOf(android.R.attr.state_checked)), intArrayOf(normalColor, selectedColor))
+        navigationView.itemIconTintList = iconSl
+        val drawable = navigationView.itemBackground
+        navigationView.itemBackground = TintHelper.createTintedDrawable(drawable, ColorUtil.withAlpha(ThemeStore.accentColor(navigationView.context), 0.2f))
+    }
+
+    fun setItemTextColors(navigationView: NavigationView, @ColorInt normalColor: Int, @ColorInt selectedColor: Int) {
+        val textSl = ColorStateList(
+                arrayOf(intArrayOf(-android.R.attr.state_checked), intArrayOf(android.R.attr.state_checked)),
+                intArrayOf(normalColor, selectedColor))
+        navigationView.itemTextColor = textSl
+    }
+
+    fun setItemIconColors(bottomNavigationView: BottomNavigationView, @ColorInt normalColor: Int, @ColorInt selectedColor: Int) {
+        val iconSl = ColorStateList(
+                arrayOf(intArrayOf(-android.R.attr.state_checked), intArrayOf(android.R.attr.state_checked)),
+                intArrayOf(normalColor, selectedColor))
+        bottomNavigationView.itemIconTintList = iconSl
+    }
+
+    fun setItemText
