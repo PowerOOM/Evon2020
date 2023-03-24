fc0f9f38248c8a588144428018faa7e589840f8c
PowerOOM <107316617+PowerOOM@users.noreply.github.com>
2023-03-24 08:38:11 +0000

    added missing package

--- appthemehelper/src/main/java/com/maxfour/appthemehelper/util/VersionUtils.kt
+++ appthemehelper/src/main/java/com/maxfour/appthemehelper/util/VersionUtils.kt
@@ -13,3 +13,29 @@ object VersionUtils {
 
     /**
      * @return true if device is running API >= 23
+     */
+    fun hasMarshmallow(): Boolean {
+        return Build.VERSION.SDK_INT >= 23
+    }
+
+    /**
+     * @return true if device is running API >= 24
+     */
+    fun hasNougat(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+    }
+
+    /**
+     * @return true if device is running API >= 24
+     */
+    fun hasNougatMR(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+    }
+
+    /**
+     * @return true if device is running API >= 26
+     */
+    fun hasOreo(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+    }
+}
